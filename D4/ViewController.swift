f3ca14e2e9c8dd6a3a0418ffb491f243e30b766c
Yao Xiao <xiaoyao901010@gmail.com>
2016-03-30 15:06:03 +0800

    Initial Commit

---
+++ D4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  D4
+//
+//  Created by 文川术 on 3/30/16.
+//  Copyright © 2016 xiaoyao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
